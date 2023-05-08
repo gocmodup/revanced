266695666a9ad2f83dafaba8c7851c1f43cc4e3a
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-05-08 17:51:07 +0700

    Update

--- build-rv.sh
+++ build-rv.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Revanced build
 source tools.sh
-release=$(wget -qO- "https://api.github.com/repos/revanced/revanced-patches/releases/latest")
+release=$(curl -s "https://api.github.com/repos/revanced/revanced-patches/releases/latest")
 asset=$(echo "$release" | jq -r '.assets[] | select(.name | test("revanced-patches.*\\.jar$")) | .browser_download_url')
-wget -q -O "$asset"
+curl -sL -O "$asset"
 ls revanced-patches*.jar >> new.txt
 rm -f revanced-patches*.jar
-release=$(wget -qO- "https://api.github.com/repos/luxysiv/revanced-nonroot/releases/latest")
+release=$(curl -s "https://api.github.com/repos/luxysiv/revanced-nonroot/releases/latest")
 asset=$(echo "$release" | jq -r '.assets[] | select(.name == "revanced-version.txt") | .browser_download_url')
-wget -q -O "$asset"
+curl -sL -O "$asset"
 if diff -q revanced-version.txt new.txt >/dev/null ; then
 rm -f *.txt
 echo "Old patch!!! Not build"
