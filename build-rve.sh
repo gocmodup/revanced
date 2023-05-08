d08bf5cf97fa79d5e8285ca45c1180c253abd0a7
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-05-08 17:30:05 +0700

    Update build-rve.sh

--- build-rve.sh
+++ build-rve.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Revanced Extended build
 source tools.sh
-release=$(curl -s "https://api.github.com/repos/inotia00/revanced-patches/releases/latest")
+release=$(wget -qO- "https://api.github.com/repos/inotia00/revanced-patches/releases/latest")
 asset=$(echo "$release" | jq -r '.assets[] | select(.name | test("revanced-patches.*\\.jar$")) | .browser_download_url')
-curl -sL -O "$asset"
+wget -q -O "$asset"
 ls revanced-patches*.jar >> new.txt
 rm -f revanced-patches*.jar
-release=$(curl -s "https://api.github.com/repos/luxysiv/revanced-nonroot/releases/latest")
+release=$(wget -qO- "https://api.github.com/repos/luxysiv/revanced-nonroot/releases/latest")
 asset=$(echo "$release" | jq -r '.assets[] | select(.name == "revanced-extended-version.txt") | .browser_download_url')
-curl -sL -O "$asset"
+wget -q -O "$asset"
 if diff -q revanced-extended-version.txt new.txt >/dev/null ; then
 rm -f *.txt
 echo "Old patch!!! Not build"
