516e148e60b9e0becc0be628b516deb6420ff285
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-05-09 23:31:51 +0700

    Update build-rve.sh

--- build-rve.sh
+++ build-rve.sh
@@ -19,11 +19,11 @@ dl_gh "inotia00"
 # Patch YouTube Extended
 get_patches_key "youtube-revanced-extended"
 get_ver "hide-general-ads" "com.google.android.youtube"
-get_apk "youtube" "youtube" "google-inc/youtube/youtube"
+get_apkmirror "youtube" "youtube" "google-inc/youtube/youtube"
 patch "youtube" "youtube-revanced-extended"
 # Patch YouTube Music Extended 
 get_patches_key "youtube-music-revanced-extended"
-get_apk_arch "youtube-music" "youtube-music" "google-inc/youtube-music/youtube-music"
+get_apkmirror_arch "youtube-music" "youtube-music" "google-inc/youtube-music/youtube-music"
 patch "youtube-music" "youtube-music-revanced-extended"
 ls revanced-patches*.jar >> revanced-extended-version.txt
 fi
